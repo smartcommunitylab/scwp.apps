d654227c95c023966d7a651c653e0aa50c91314d
Andrea Panizza <panizza01@gmail.com>
2014-05-07 17:00:57 +0200

    first app draft

---
+++ ViaggiaTrentino/ViaggiaTrentino/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.Resources;
+using System.Windows;
+using System.Windows.Markup;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using ViaggiaTrentino.Resources;
+
+namespace ViaggiaTrentino
+{
+  public partial class App : Application
+  {
+
+    public static PhoneApplicationFrame RootFrame { get; private set; }
+
+    public App()
+    {
+      InitializeComponent();
+
+      if (Debugger.IsAttached)
+      {
+        Application.Current.Host.Settings.EnableFrameRateCounter = true;
+        PhoneApplicationService.Current.UserIdleDetectionMode = IdleDetectionMode.Disabled;
+      }
+
+    }
+
+  }
+}
---
+++ ViaggiaTrentino/ViaggiaTrentino/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,54 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ViaggiaTrentino.ViewModels
+{
+  public class MainPageViewModel : PropertyChangedBase
+  {
+    private string name;
+    private bool isEnabled;
+
+
+    public MainPageViewModel()
+    {
+      name = "COOOOOOOP";
+      //MessageBox.Show("I'm the MainView!");
+    }
+    public bool IsEnabled
+    {
+      get { return isEnabled; }
+      set
+        {
+            isEnabled = value;
+            NotifyOfPropertyChange(() => IsEnabled);
+            NotifyOfPropertyChange(() => CanShowPower);
+        }
+    }
+    public string Name
+    {
+      get { return name; }
+      set
+      {
+        name = value;
+        NotifyOfPropertyChange(() => Name);
+      }
+    }
+    
+    public bool CanShowPower
+    {
+      get { return IsEnabled; }
+    }
+    public void ShowPower()
+    {
+      MessageBox.Show("moar power");
+    }
+
+
+
+  }
+}
