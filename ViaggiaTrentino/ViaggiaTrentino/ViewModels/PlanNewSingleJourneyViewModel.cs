84f402c9044b22126cd60729d628fd5729b4b18b
TheFerrango <llorenzo@outlook.com>
2014-05-09 11:02:07 +0200

    added strings for PlanNewSingleJourney

--- ViaggiaTrentino/ViaggiaTrentino/ViewModels/PlanNewSingleJourneyViewModel.cs
+++ ViaggiaTrentino/ViaggiaTrentino/ViewModels/PlanNewSingleJourneyViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Caliburn.Micro;
+using Models.MobilityService.Journeys;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,13 +12,49 @@ namespace ViaggiaTrentino.ViewModels
   {
     private readonly INavigationService navigationService;
     bool isSettingsShown;
+    SingleJourney journey;
     DateTime departureDate;
+    string fromText;
+    string toText;
+
 
     
     public PlanNewSingleJourneyViewModel(INavigationService navigationService)
     {
       this.navigationService = navigationService;
       departureDate = DateTime.Now;
+      journey = new SingleJourney();
+      
+    }
+
+    public string FromText
+    {
+      get { return fromText; }
+      set
+      {
+        fromText = value;
+        NotifyOfPropertyChange(() => FromText);
+      }
+    }
+
+    public string ToText
+    {
+      get { return toText; }
+      set
+      {
+        fromText = value;
+        NotifyOfPropertyChange(() => ToText);
+      }
+    }
+
+    public SingleJourney Journey
+    {
+      get { return journey; }
+      set
+      {
+        journey = value;
+        NotifyOfPropertyChange(() => Journey);
+      }
     }
 
     public DateTime DepartureDateTime
@@ -39,6 +76,11 @@ namespace ViaggiaTrentino.ViewModels
         NotifyOfPropertyChange(() => IsSettingsShown);
 
       }
+    }
+
+    public void PlanNewJourney()
+    {
+      //finalize SingleJourneyObject and proceed to post
     }
   }
 }
