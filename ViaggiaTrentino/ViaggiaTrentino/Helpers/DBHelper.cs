a91e687dd3281718d47e3ac58a69adfeb97d5763
TheFerrango <llorenzo@outlook.com>
2014-05-16 11:28:29 +0200

    DBHelper
    *added class with basic method stubs

---
+++ ViaggiaTrentino/ViaggiaTrentino/Helpers/DBHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViaggiaTrentino.Helpers
+{
+  public class DBHelper
+  {
+    string dbName;
+
+    public DBHelper(string DBName)
+    {
+      dbName = DBName;
+    }
+
+    public DBHelper()
+    {
+      dbName = "SCDB.sqlite";
+    }
+
+    private bool OpenConnection()
+    {
+      return true
+    }
+
+    private bool CloseConnection()
+    {
+      return true;
+    }
+
+  }
+}
