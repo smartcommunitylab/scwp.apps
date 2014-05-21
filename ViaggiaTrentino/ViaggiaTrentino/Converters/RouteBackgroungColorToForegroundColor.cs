dddd56b172401c7781f4dc2da1312411ef979445
Andrea Panizza <panizza01@gmail.com>
2014-05-21 17:57:45 +0200

    added missing files

---
+++ ViaggiaTrentino/ViaggiaTrentino/Converters/RouteBackgroungColorToForegroundColor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ViaggiaTrentino.Converters
+{
+  public class RouteBackgroungColorToForegroundColor : IValueConverter
+  {
+    public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+      if (value is string)
+      {
+        switch (value as string)
+        {
+          case "#FFF": return "#A5000000";
+          case "#FFFFFF": return "#A5000000";
+          default: return "#A5FFFFFF";
+        }
+      }
+      return null;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ ViaggiaTrentino/ViaggiaTrentino/ViewModels/TimetablePageViewModel.cs
@@ -0,0 +1,92 @@
+﻿using Caliburn.Micro;
+using CommonHelpers;
+using System.Linq;
+using System.Collections.Generic;
+using DBManager;
+using MobilityServiceLibrary;
+using Models.MobilityService;
+using System.Collections.ObjectModel;
+using Newtonsoft.Json;
+using System;
+
+namespace ViaggiaTrentino.ViewModels
+{
+  public class TimetablePageViewModel : Screen
+  {
+    private readonly INavigationService navigationService;
+    private AgencyType agencyID;
+    private string routeID;
+    private ObservableCollection<DBManager.DBModels.RouteName> routeNames;
+
+    DBManager.DBModels.RouteName selectedRouteName;
+
+    public TimetablePageViewModel(INavigationService navigationService)
+    {
+      this.navigationService = navigationService;    
+    }
+
+    public ObservableCollection<DBManager.DBModels.RouteName> RouteNames
+    {
+      get { return routeNames; }
+      set
+      {
+        routeNames = value;
+        NotifyOfPropertyChange(() => RouteNames);
+      }
+    }
+
+    public AgencyType AgencyID
+    {
+      get { return agencyID; }
+      set { agencyID = value; }
+    }
+
+    public string RouteID
+    {
+      get { return routeID; }
+      set { routeID = value; }
+    }
+
+    public DBManager.DBModels.RouteName SelectedRouteName
+    {
+      get { return selectedRouteName; }
+      set
+      {
+        selectedRouteName = value;
+        NotifyOfPropertyChange(() => SelectedRouteName);
+        GetTimetableFromDB();
+      }
+    }
+
+    private void GetTimetableFromDB()
+    {
+      using (DBHelper dbh = new DBHelper())
+      {
+        
+        var calendar = dbh.GetCalendar(EnumConverter.ToEnumString<AgencyType>(agencyID), SelectedRouteName.RouteID).CalendarEntries;
+        var results = JsonConvert.DeserializeObject<Dictionary<string, string>>(calendar);
+
+        string key = DateTime.Now.ToString("yyyyMMdd");
+        if (results.ContainsKey(key) && results[key] != null)
+        {
+          string name = String.Format("{0}_{1}", SelectedRouteName.RouteID, results[key]);
+          var timetable = dbh.GetRouteCalendar(name);
+        }
+      }
+    }
+
+    protected override void OnViewLoaded(object view)
+    {
+      base.OnViewLoaded(view);
+      using (DBHelper dbh = new DBHelper())
+      {
+        RouteNames = new ObservableCollection<DBManager.DBModels.RouteName>(
+          dbh.GetRoutesNames(EnumConverter.ToEnumString<AgencyType>(agencyID)).Where(
+            x=>(x.RouteID.StartsWith(routeID) || x.RouteID.StartsWith("0"+routeID))
+          ));
+
+        SelectedRouteName = RouteNames.FirstOrDefault();
+      }
+    }
+  }
+}
