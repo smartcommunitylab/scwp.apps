64b6ce611e8323141b232b08e19714d19497e135
Andrea Panizza <panizza01@gmail.com>
2014-07-01 16:32:13 +0200

    change some ui elements

---
+++ ViaggiaTrentino/ViaggiaTrentino/Converters/BorderColorToggleBlackAndWhiteConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ViaggiaTrentino.Converters
+{
+  public class BorderColorToggleBlackAndWhiteConverter : IValueConverter
+  {
+    public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+      if (value is string)
+      {
+        switch (value as string)
+        {
+          case "#FFF": return "#FF000000";
+          case "#FFFFFF": return "#FF000000";
+          case "#000": return "#FFFFFFFF";
+          case "#000000": return "#FFFFFFFF";
+          default: return value;
+        }
+      }
+      return null;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
