1778fd2539498ad8dead0f07f7c50c018b47dba1
Andrea Panizza <panizza01@gmail.com>
2014-08-05 10:12:15 +0200

    different client ID and secret for each app
    * restored default pushpin

--- ViaggiaTrentino/ViaggiaRovereto/AppSpecificSettings.cs
+++ ViaggiaTrentino/ViaggiaRovereto/AppSpecificSettings.cs
@@ -18,8 +18,8 @@ namespace ViaggiaTrentino
     
     public static void AppSpecificInitialize()
     {
-      clientId = "52482826-891e-4ee0-9f79-9153a638d6e4";
-      clientSecret = "f3ea5378-43ba-42c3-b2bf-5f7cd10b6e6e";
+      clientId = "0a9531e5-6c95-4291-848f-1ed8a5914379";
+      clientSecret = "efc438a8-28ea-457c-9f4c-9f6b69ec4499";
       redirectUrl = "http://localhost";
       serverUrl = "https://vas-dev.smartcampuslab.it/";
     }
