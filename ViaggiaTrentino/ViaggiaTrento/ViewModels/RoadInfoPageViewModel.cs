14c9dabd47ead6c96d551d4bbfc6d734ac1fb63a
Andrea Panizza <panizza01@gmail.com>
2014-08-06 16:37:36 +0200

    removed debug stuff

--- ViaggiaTrentino/ViaggiaTrento/ViewModels/RoadInfoPageViewModel.cs
+++ ViaggiaTrentino/ViaggiaTrento/ViewModels/RoadInfoPageViewModel.cs
@@ -30,12 +30,6 @@ namespace ViaggiaTrentino.ViewModels
       this.navigationService = navigationService;
       ptl = new PublicTransportLibrary(Settings.AppToken.AccessToken, Settings.ServerUrl);
       NoResults = false;
-      string j1, j2, j3, j4;
-      j1 = "{ \"agencyId\":\"COMUNE_DI_ROVERETO\", \"road\":{\"note\":\"\",\"lat\":\"45.89380160943986\",\"lon\":\"11.043856143951416\",\"streetCode\":\"290\",\"street\":\"CORSO BETTINI A.\",\"fromNumber\":\"\",\"toNumber\":\"\",\"fromIntersection\":\"\",\"toIntersection\":\"\"},\"changeTypes\":[ \"ROAD_BLOCK\"],\"id\":\"612005_290\", \"type\":null, \"entity\":null,  \"description\":\"UFFICIO ATTIVITA' PRODUTTIVE: DIVIETO DI TRANSITO E DI SOSTA CON RIMOZIONE COATTA IN CORSO BETTINI, IN VIALE TRENTO E NELLE STRADE LIMITROFE A ROVERETO PER LO SVOLGIMENTO DEL MERCATO SETTIMANALE DEL MARTEDI'.\",  \"from\":1372543200000,\"to\":1378764000000,  \"creatorId\":\"default\", \"creatorType\":\"SERVICE\", \"effect\":\"Temporanea\", \"note\":null},";
-      j2 = "{ \"agencyId\":\"COMUNE_DI_ROVERETO\", \"road\":{\"note\":\"\",\"lat\":\"45.88976919245778\",\"lon\":\"11.036818027496338\",\"streetCode\":\"290\",\"street\":\"CORSO BETTINI A.\",\"fromNumber\":\"\",\"toNumber\":\"\",\"fromIntersection\":\"\",\"toIntersection\":\"\"},\"changeTypes\":[ \"OTHER\",\"ROAD_BLOCK\"],\"id\":\"612005_290\", \"type\":null, \"entity\":null,  \"description\":\"UFFICIO ATTIVITA' PRODUTTIVE: DIVIETO DI TRANSITO E DI SOSTA CON RIMOZIONE COATTA IN CORSO BETTINI'.\",  \"from\":1372543200000,\"to\":1378764000000,  \"creatorId\":\"default\", \"creatorType\":\"SERVICE\", \"effect\":\"Temporanea\", \"note\":null},";
-      j3 = "{ \"agencyId\":\"COMUNE_DI_ROVERETO\", \"road\":{\"note\":\"\",\"lat\":\"45.884481802178996\",\"lon\":\"11.048061847686768\",\"streetCode\":\"290\",\"street\":\"CORSO BETTINI A.\",\"fromNumber\":\"\",\"toNumber\":\"\",\"fromIntersection\":\"\",\"toIntersection\":\"\"},\"changeTypes\":[ \"DRIVE_CHANGE\",\"PARKING_BLOCK\",\"ROAD_BLOCK\",],\"id\":\"612005_290\", \"type\":null, \"entity\":null,  \"description\":\"UFFICIO ATTIVITA' PRODUTTIVE'.\",  \"from\":1372543200000,\"to\":1378764000000,  \"creatorId\":\"default\", \"creatorType\":\"SERVICE\", \"effect\":\"Temporanea\", \"note\":null},";
-      j4 = "{ \"agencyId\":\"COMUNE_DI_ROVERETO\", \"road\":{\"note\":\"\",\"lat\":\"45.90025286764715\",\"lon\":\"11.037590503692627\",\"streetCode\":\"290\",\"street\":\"CORSO BETTINI A.\",\"fromNumber\":\"\",\"toNumber\":\"\",\"fromIntersection\":\"\",\"toIntersection\":\"\"},\"changeTypes\":[ \"PARKING_BLOCK\",\"ROAD_BLOCK\",\"DRIVE_CHANGE\",\"OTHER\"],\"id\":\"612005_290\", \"type\":null, \"entity\":null,  \"description\":\"UFFICIO ATTIVITA' PRODUTTIVE: DIVIETO DI TRANSITO E DI SOSTA CON RIMOZIONE COATTA IN CORSO BETTINI, IN VIALE TRENTO E NELLE STRADE LIMITROFE A ROVERETO PER LO SVOLGIMENTO DEL MERCATO SETTIMANALE DEL MARTEDI'.\",  \"from\":1372543200000,\"to\":1378764000000,  \"creatorId\":\"default\", \"creatorType\":\"SERVICE\", \"effect\":\"Temporanea\", \"note\":null},";
-      json = "[" + j1 + j2 + j3 + j4 + "]";
     }
 
     private long DateTimeToEpoch(DateTime dt)
@@ -73,9 +67,6 @@ namespace ViaggiaTrentino.ViewModels
         App.LoadingPopup.Show();
         await Settings.RefreshToken();
         results = await ptl.GetRoadInfoByAgency(Settings.ParkingAgencyId, DateTimeToEpoch(DateTime.Now), DateTimeToEpoch(DateTime.Now.AddDays(7)));
-#if DEBUG
-        results.AddRange(JsonConvert.DeserializeObject<List<AlertRoad>>(json));
-#endif
       }
       finally
       {
