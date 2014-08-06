71552cdee74c0d94212899194a689f25716ea6b9
Andrea Panizza <panizza01@gmail.com>
2014-08-06 12:06:59 +0200

    fixed guided tour

--- ViaggiaTrentino/ViaggiaTrento/Views/MainPageView.xaml.cs
+++ ViaggiaTrentino/ViaggiaTrento/Views/MainPageView.xaml.cs
@@ -9,9 +9,9 @@ using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using System.Windows.Controls.Primitives;
 using Caliburn.Micro;
-using GuidedTour;
 using RateMyApp.Controls;
 using System.Windows.Media;
+using GuidedTour;
 
 namespace ViaggiaTrentino.Views
 {
@@ -55,12 +55,15 @@ namespace ViaggiaTrentino.Views
     {
       if (overrideCheck || (!Settings.IsTourAlreadyShown && Settings.IsLogged))
       {
-        tour = new Tour(Application.Current.Host.Content.ActualWidth, Application.Current.Host.Content.ActualHeight);
-        //tour.Clear();
+        tour = new Tour(Application.Current.Host.Content.ActualWidth, Application.Current.Host.Content.ActualHeight)
+        {
+          ButtonsAlignment = System.Windows.VerticalAlignment.Bottom
+        };
+        tour.Clear();
 
         foreach (HubTile ht in HubTilePanel.Children.OfType<HubTile>())
-          if(ht.Visibility == System.Windows.Visibility.Visible)
-            tour.Add(new TourElement(LayoutRoot, ht, (ht as HubTile).Tag as string, (ht as HubTile).Title, new Point(0,32)));
+          if (ht.Visibility == System.Windows.Visibility.Visible)
+            tour.Add(new TourElement(LayoutRoot, ht, (ht as HubTile).Tag as string, (ht as HubTile).Title, new Point(0, 32)));
 
         tour.TourStarted += tour_TourStarted;
         tour.TourCompleted += tour_TourCompleted;
@@ -80,14 +83,14 @@ namespace ViaggiaTrentino.Views
     //do not delete this function, even if its empty
     void tour_TourProgressChanged(int percentage)
     {
-      //ScrollViewer sv = ContentPanel.Children.OfType<ScrollViewer>().First();
-      //if (tour.CurrentElement.Position.Y + tour.CurrentElement.Height > Application.Current.Host.Content.ActualHeight)
-      //{
-      //  if (tour.CurrentElement.Position.Y + tour.CurrentElement.Height > Application.Current.Host.Content.ActualHeight + sv.VerticalOffset)
-      //    sv.ScrollToVerticalOffset(tour.CurrentElement.Position.Y - tour.CurrentElement.Height - 32);
+      ScrollViewer sv = ContentPanel.Children.OfType<ScrollViewer>().First();
+      if (tour.CurrentElement.Position.Y + tour.CurrentElement.Height > Application.Current.Host.Content.ActualHeight)
+      {
+        if (tour.CurrentElement.Position.Y + tour.CurrentElement.Height > Application.Current.Host.Content.ActualHeight + sv.VerticalOffset)
+          sv.ScrollToVerticalOffset(tour.CurrentElement.Position.Y - tour.CurrentElement.Height - 32);
 
-      //  tour.CurrentElement.Position = new Point(tour.CurrentElement.Position.X, tour.CurrentElement.Position.Y - sv.VerticalOffset);
-      //}
+        tour.CurrentElement.Position = new Point(tour.CurrentElement.Position.X, tour.CurrentElement.Position.Y - sv.VerticalOffset);
+      }
     }
 
     void tour_TourStarted()
