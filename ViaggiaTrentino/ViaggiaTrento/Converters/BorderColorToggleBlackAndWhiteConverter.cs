47ab73a97e80712b652a4d6e29431c72c3c16e73
Andrea Panizza <panizza01@gmail.com>
2014-08-06 17:26:34 +0200

    added border for black routes

--- ViaggiaTrentino/ViaggiaTrento/Converters/BorderColorToggleBlackAndWhiteConverter.cs
+++ ViaggiaTrentino/ViaggiaTrento/Converters/BorderColorToggleBlackAndWhiteConverter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows.Data;
+using System.Windows.Media;
 
 namespace ViaggiaTrentino.Converters
 {
@@ -7,18 +8,28 @@ namespace ViaggiaTrentino.Converters
   {
     public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
     {
+      string color = null;
+
+      if (value is SolidColorBrush)
+        color = (value as SolidColorBrush).Color.ToString();
+
       if (value is string)
+        color = value as string;
+
+      if (color != null)
       {
-        switch (value as string)
+        switch (color as string)
         {
-          case "#FFF": 
+          case "#FFF":
+          case "#FFFFFFFF":
           case "#FFFFFF": return "#FF000000";
-          case "#000": 
+          case "#000":
+          case "#FF000000":
           case "#000000": return "#FFFFFFFF";
-          default: return value;
+          default: return color;
         }
       }
-      return null;
+      return color;
     }
 
     public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
